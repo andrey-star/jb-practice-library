77968c0a29975d64b9ebfc9e0aacd20b42175c65
Andrey <andreystar2403@gmail.com>
2021-03-13 15:48:19 +0300

    Add book addition to readers library

--- src/main/java/com/andreystar/library/controller/ReaderRestController.java
+++ src/main/java/com/andreystar/library/controller/ReaderRestController.java
@@ -17,10 +17,13 @@ public class ReaderRestController {
 	
 	@Resource
 	private final ReaderService readerService;
+	@Resource
+	private final BookService bookService;
 	
 	@Autowired
-	public ReaderRestController(ReaderService readerService) {
+	public ReaderRestController(ReaderService readerService, BookService bookService) {
 		this.readerService = readerService;
+		this.bookService = bookService;
 	}
 	
 	@GetMapping
@@ -58,6 +61,30 @@ public class ReaderRestController {
 		}
 		readerService.deleteById(readerId);
 		return "Deleted reader with id: " + readerId;
+	}
+	
+	@GetMapping("/{readerId}/books")
+	public Set<Book> getBooks(@PathVariable int readerId) {
+		Reader reader = readerService.findById(readerId);
+		if (reader == null) {
+			throw new RuntimeException("Reader id not found: " + readerId);
+		}
+		return reader.getBooks();
+	}
+	
+	@PutMapping("/{readerId}/books/{bookId}")
+	public Reader addBook(@PathVariable int readerId, @PathVariable int bookId) {
+		Reader reader = readerService.findById(readerId);
+		if (reader == null) {
+			throw new RuntimeException("Reader id not found: " + readerId);
+		}
+		Book book = bookService.findById(bookId);
+		if (book == null) {
+			throw new RuntimeException("Book id not found: " + bookId);
+		}
+		reader.addBook(book);
+		readerService.save(reader);
+		return reader;
 	}
 	
 }
